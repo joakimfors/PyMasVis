e6987b6d77922db834d4a2ff8e2dc2d23c312b35
Joakim Fors <joakim@joakimfors.org>
2016-01-09 14:09:26 +0100

    Increment version

--- src/analyze.py
+++ src/analyze.py
@@ -44,7 +44,7 @@ from matplotlib.ticker import MaxNLocator, FuncFormatter, ScalarFormatter, Forma
 from PIL import Image
 
 
-VERSION="0.9.5"
+VERSION="0.9.6"
 
 class Timer(object):
 	def __init__(self, verbose=False):
