58e8159955f8cfe84452943df2198266be1d0d02
Joakim Fors <joakim@joakimfors.org>
2016-04-07 13:06:22 +0200

    Fix indentation bug in gcd

--- src/analyze.py
+++ src/analyze.py
@@ -288,7 +288,7 @@ def gcd(a, b):
 	"""Return greatest common divisor using Euclid's Algorithm."""
 	while b:
 		a, b = b, a % b
-		return a
+	return a
 
 def lcm(a, b):
 	"""Return lowest common multiple."""
