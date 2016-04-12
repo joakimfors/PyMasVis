f1693e4fcf18acd19270fd8f589d0e69df7c678f
Joakim Fors <joakim@joakimfors.org>
2016-04-12 08:50:25 +0200

    Add UI feedback when processing and AJAX form submission

--- src/webapp.py
+++ src/webapp.py
@@ -5,6 +5,7 @@ import time
 import random
 import string
 import logging
+import resource
 import multiprocessing
 import atexit
 import flask
@@ -12,23 +13,15 @@ import eventlet
 import eventlet.wsgi
 import analyze as pymasvis
 
-import resource
-
 from eventlet.green import zmq
-#from werkzeug.serving import run_with_reloader
-#from werkzeug.debug import DebuggedApplication
-from flask import Flask, render_template, session, request, send_file, abort
+from flask import Flask, render_template, session, request, send_file, abort, flash
 from flask_socketio import SocketIO, emit, join_room, rooms
 from PIL import Image
 
 
-
 app = Flask(__name__)
-app.debug = True
-app.secret_key = 'foobar'
+app.secret_key = 'Oo1Shu*nae|daisi7kaePh#ae7ef6ooG'
 socketio = SocketIO(app, async_mode='eventlet')
-pymasvis.log.setLevel(logging.INFO)
-
 pool = None
 thread = None
 
@@ -48,32 +41,41 @@ def before_request():
 
 @app.route('/')
 def index():
-	#print dir(request)
-	#print('/', request.sid)
-	print 'Hell that session user:', session
-	return render_template('index.html', count=0)
+	return render_template('index.html', debug=app.debug)
 
 
 @app.route('/analyze', methods=['GET', 'POST'])
 def analyze():
 	global pool
+	print 'XHR?', request.is_xhr
 	if request.method == 'POST':
-		if 'file' not in request.files:
+		try:
+			f = request.files['file']
+		except KeyError as e:
+			flash('Missing file parameter')
+			print e
 			abort(500)
-		f = request.files['file']
-		print f
+		if not f:
+			flash('No file found in request')
+			print f
+			abort(500)
 		infile = f.filename
-		print infile
 		inbuffer = f.stream.read()
 		result = pool.apply_async(render, (infile, inbuffer, session['uid']))
 		while not result.ready():
-			#gevent.sleep(0.5)
 			eventlet.sleep(0.5)
 		imgbuf = result.get(timeout=1)
-		r = send_file(imgbuf, as_attachment=True, attachment_filename="%s-pymasvis.png" % infile, mimetype='image/png')
-		print r
-		print dir(r)
-		return r
+		if type(imgbuf) is not io.BytesIO:
+			flash('Failed to analyze file')
+			abort(500)
+		return send_file(imgbuf, as_attachment=True, attachment_filename="%s-pymasvis.png" % infile, mimetype='image/png')
+
+
+@app.route('/status')
+def status():
+	result = "Memory self: %6d KiB, children: %6d KiB" % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024, resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss/1024)
+	print result
+	return result, 200
 
 
 @socketio.on('connect')
@@ -89,44 +91,39 @@ def on_disconnect():
 
 @socketio.on('join')
 def on_join(message):
-	print('Hej', message)
 	if 'room' in message:
 		join_room(message['room'])
 
 
-def cleanup():
+def pool_cleaner():
 	global pool
 	pool.close()
 	pool.join()
 
 
 def worker():
-	"""Example of how to send server generated events to clients."""
-	print "Boom!"
 	ctx = zmq.Context()
 	sock = ctx.socket(zmq.PULL)
-	sock.bind('tcp://127.0.0.1:1234')
+	sock.bind('tcp://127.0.0.1:64646')
 	while True:
-		print "I'm in a loop!"
-		#eventlet.sleep(10)
 		msg = sock.recv_pyobj()
-		print 'Got:', msg
-		#sock.send('OK')
-		print "Memory self: %6d KiB, children: %6d KiB" % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024, resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss/1024)
-		socketio.emit('event', {'data': 'Server generated event', 'message': msg[1]}, room=msg[0])
+		socketio.emit('progress', {'message': msg[1], 'status': msg[2]}, room=msg[0])
 
 
 def render(infile, inbuffer, uid):
 	print 'Render req from uid:', uid
 	ctx = zmq.Context()
 	sock = ctx.socket(zmq.PUSH)
-	sock.connect('tcp://127.0.0.1:1234')
-	sock.send_pyobj([uid, 'Hello, world!'])
-	#sock.recv()
+	sock.connect('tcp://127.0.0.1:64646')
+	sock.send_pyobj([uid, 'Analysing...', 'start'])
 	def callback(event, tid, desc=None, secs=None):
 		if event == 'start':
-			sock.send_pyobj([uid, desc])
+			sock.send_pyobj([uid, desc, 'inprogress'])
 	track = pymasvis.load_file(infile, inbuffer)
+	if type(track) is int:
+		sock.send_pyobj([uid, 'Failed to find audiostream in file', 'finished'])
+		eventlet.sleep(2)
+		return 0
 	result = pymasvis.analyze(track, callback=callback)
 	detailed, overview = pymasvis.render(track, result, track['metadata']['name'], render_overview=False, callback=callback)
 	img = Image.open(detailed)
@@ -136,19 +133,26 @@ def render(infile, inbuffer, uid):
 	detailed.close()
 	img.close()
 	imgbuf.seek(0)
-	sock.send_pyobj([uid, 'Goodbye, world!'])
-	#sock.recv()
+	sock.send_pyobj([uid, 'Done!', 'finished'])
 	return imgbuf
 
 
-#@run_with_reloader
-def main():
+def main(host=None, port=None):
 	global pool
 	pool = multiprocessing.Pool()
-	atexit.register(cleanup)
-	#gevent.wsgi.WSGIServer(('', 5000), DebuggedApplication(app)).serve_forever()
-	#eventlet.wsgi.server(eventlet.listen(('', 5000)), DebuggedApplication(app))
-	socketio.run(app, debug=True)
+	atexit.register(pool_cleaner)
+	socketio.run(app, host=host, port=port, debug=app.debug)
 
 if __name__ == '__main__':
-	main()
+	import argparse
+	parser = argparse.ArgumentParser(description="Web app for PyMasVis")
+	parser.add_argument('-d', '--debug', action='store_true', help="debug info")
+	parser.add_argument('--host', type=str, default=None, help="listen address")
+	parser.add_argument('--port', type=int, default=None, help="listen port")
+	args = parser.parse_args()
+	if args.debug:
+		app.debug = True
+		pymasvis.log.setLevel(logging.INFO)
+	else:
+		app.debug = False
+	main(args.host, args.port)
